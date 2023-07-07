3eb51438f79c61db90f5139ffba4a3923d30dd04
AmandavrVitat <amanda.rocha@vitat.com.br>
2023-07-06 21:38:36 -0300

    init

---
+++ lib/vitat/css.rb
@@ -0,0 +1,8 @@
+require "vitat/css/version"
+
+module Vitat
+  module Css
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/vitat/css/version.rb
@@ -0,0 +1,5 @@
+module Vitat
+  module Css
+    VERSION = "0.1.3"
+  end
+end
